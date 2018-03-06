b41827598f6b3c4ff8d0f6890fd463d462859299
Carolyne Peelman <r0458882@ucll.be>
2018-03-06 21:20:51 +0100

    Implementation TemperatureScale

--- TemperatureConverter/TemperatureConverter/TemperatureScale.cs
+++ TemperatureConverter/TemperatureConverter/TemperatureScale.cs
@@ -13,4 +13,49 @@ namespace TemperatureConverter
         double ConvertToKelvin(double temperature);
         double ConvertFromKelvin(double temperature);
     }
+
+    class KelvinTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Kelvin";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature;
+        }
+    }
+
+    class CelsiusTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Celsius";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature - 273.15;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature + 273.15;
+        }
+    }
+
+    class FahrenheitTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Fahrenheit";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return (temperature - 273.15) * 1.8 + 32;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return (temperature - 32) / 1.8 + 273.15;
+        }
+    }
 }
