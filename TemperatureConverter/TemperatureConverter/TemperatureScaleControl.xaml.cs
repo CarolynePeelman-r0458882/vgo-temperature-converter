3b474c6881acc0fe9ed63aecc49895afc4358158
Carolyne Peelman <r0458882@ucll.be>
2018-03-07 17:14:08 +0100

    Two way Binding

--- TemperatureConverter/TemperatureConverter/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/TemperatureConverter/TemperatureScaleControl.xaml.cs
@@ -24,5 +24,21 @@ namespace View
         {
             InitializeComponent();
         }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(TemperatureScaleControl), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
     }
 }
