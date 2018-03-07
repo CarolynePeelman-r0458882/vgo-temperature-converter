801b15d6a52662a576e0cad94e1fc9f823d8bc0a
Carolyne Peelman <r0458882@ucll.be>
2018-03-07 15:54:51 +0100

    Converter VM Cell

---
+++ TemperatureConverter/Cells/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Cells
+{
+    public class Cell<T> : INotifyPropertyChanged
+    {
+        private T contents;
+
+        public Cell(T initialContents = default(T))
+        {
+            this.contents = initialContents;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return contents;
+            }
+            set
+            {
+                this.contents = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(value));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
